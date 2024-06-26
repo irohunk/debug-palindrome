ca21bc90f2cf1c3e3707d55e79fe67de0bfb786e
Rohan Kulkarni <irohunk@gmail.com>
2024-06-26 11:56:37 -0400

    removed bugs

--- palindrome.js
+++ palindrome.js
@@ -1,24 +1,31 @@
 // FUNCTION IMPLEMENTATION (MULTIPLE BUGS)
 const isPalindrome = function(str) {
   const noSpaces = str.split(" ").join("");
-  const midIndex = Math.floor(noSpaces.length/2);
-  const lastIndex = noSpaces.length - 1;
+  // console.log(noSpaces);
+  const strLowCase = noSpaces.toLowerCase();
+  const midIndex = Math.ceil(strLowCase.length / 2);
+  // console.log(midIndex);
+  const lastIndex = strLowCase.length - 1;
+  // console.log(lastIndex);
 
   for (let i = 0; i < midIndex; i++) {
-    if (str[i] !== str[lastIndex - i]) return false;
+    if (strLowCase[i] !== strLowCase[lastIndex - i]) {
+      return false;
+    }
   }
-}
+  return true;
+};
 
 // Assertion Function
 const assertPalindrome = function(word, expected) {
-  console.log(`Testing isPalindrome(\"${word}\"):`);
+  console.log(`Testing isPalindrome ("${word}"):`);
   const actual = isPalindrome(word);
   if (actual === expected) {
     console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ function returned ${actual}\n`);
   } else {
     console.log("\x1b[31m%s\x1b[0m", `\tFAIL 🛑 function returned ${actual} (expected ${expected})\n`);
   }
-}
+};
 
 
 // TEST CODE
@@ -31,5 +38,5 @@ assertPalindrome('fluff', false);
 assertPalindrome('just some random words', false);
 
 // Bonus / Stretch: Uncomment these tests and figure out why these are also failing
-// assertPalindrome('Kayak', true);
-// assertPalindrome('a santa at NASA', true);
+assertPalindrome('Kayak', true);
+assertPalindrome('a santa at NASA', true);
